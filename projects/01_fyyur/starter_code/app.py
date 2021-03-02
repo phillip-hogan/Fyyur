8329ac4d65d80031e10e3ec5e610c9c09b2b416c
Phillip Hogan <phillip.jr.hogan@gmail.com>
2021-03-02 07:04:35 +0000

    Finshed edit artist and venue routes

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -242,6 +242,7 @@ def create_venue_form():
 
 @app.route('/venues/create', methods=['POST'])
 def create_venue_submission():
+    # TODO use WTForms
     try:
         name = request.form['name']
         city = request.form['city']
@@ -294,7 +295,6 @@ def delete_venue(venue_id):
 
         db.session.delete(venue)
         db.session.commit()
-        # TODO flashes don't show
         flash('Venue ' + venue.name + ' was successfully deleted!')
     except:
         db.session.rollback()
@@ -309,13 +309,13 @@ def delete_venue(venue_id):
 #  ----------------------------------------------------------------
 
 
-@ app.route('/artists')
+@app.route('/artists')
 def artists():
     data = Artist.query.all()
     return render_template('pages/artists.html', artists=data)
 
 
-@ app.route('/artists/search', methods=['POST'])
+@app.route('/artists/search', methods=['POST'])
 def search_artists():
     # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
     # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
@@ -331,7 +331,7 @@ def search_artists():
     return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
 
 
-@ app.route('/artists/<int:artist_id>')
+@app.route('/artists/<int:artist_id>')
 def show_artist(artist_id):
     # TODO sort past and upcoming shows.
 
@@ -389,7 +389,7 @@ def show_artist(artist_id):
 #  ----------------------------------------------------------------
 
 
-@ app.route('/artists/<int:artist_id>/edit', methods=['GET'])
+@app.route('/artists/<int:artist_id>/edit', methods=['GET'])
 def edit_artist(artist_id):
     artist = Artist.query.filter_by(id=artist_id).first_or_404()
     form = ArtistForm(obj=artist)
@@ -397,15 +397,39 @@ def edit_artist(artist_id):
     return render_template('forms/edit_artist.html', form=form, artist=artist)
 
 
-@ app.route('/artists/<int:artist_id>/edit', methods=['POST'])
+@app.route('/artists/<int:artist_id>/edit', methods=['POST'])
 def edit_artist_submission(artist_id):
-    # TODO: take values from the form submitted, and update existing
-    # artist record with ID <artist_id> using the new attributes
+    artist = Artist.query.filter_by(id=artist_id).first_or_404()
+    form = ArtistForm(request.form)
+
+    try:
+        artist.name = form.name.data
+        artist.city = form.city.data
+        artist.state = form.state.data
+        artist.phone = form.phone.data
+        artist.genres = form.genres.data
+        artist.facebook_link = form.facebook_link.data
+        artist.image_link = form.image_link.data
+        artist.website = form.website.data
+        artist.seeking_venue = form.seeking_venue.data
+        artist.seeking_description = form.seeking_description.data
+
+        # raise ValueError('A very specific bad thing happened.')
+
+        db.session.commit()
+        flash('Artist ' + artist.name + ' was successfully updated!')
+    except:
+        db.session.rollback()
+        print(sys.exc_info())
+        flash('An error occurred. Artist ' +
+              request.form['name'] + ' could not be updated.')
+    finally:
+        db.session.close()
 
     return redirect(url_for('show_artist', artist_id=artist_id))
 
 
-@ app.route('/venues/<int:venue_id>/edit', methods=['GET'])
+@app.route('/venues/<int:venue_id>/edit', methods=['GET'])
 def edit_venue(venue_id):
     venue = Venue.query.filter_by(id=venue_id).first_or_404()
     form = VenueForm(obj=venue)
@@ -413,24 +437,51 @@ def edit_venue(venue_id):
     return render_template('forms/edit_venue.html', form=form, venue=venue)
 
 
-@ app.route('/venues/<int:venue_id>/edit', methods=['POST'])
+@app.route('/venues/<int:venue_id>/edit', methods=['POST'])
 def edit_venue_submission(venue_id):
-    # TODO: take values from the form submitted, and update existing
-    # venue record with ID <venue_id> using the new attributes
+    venue = Venue.query.filter_by(id=venue_id).first_or_404()
+    form = VenueForm(request.form)
+
+    try:
+        venue.name = form.name.data
+        venue.city = form.city.data
+        venue.state = form.state.data
+        venue.address = form.address.data
+        venue.phone = form.phone.data
+        venue.genres = form.genres.data
+        venue.facebook_link = form.facebook_link.data
+        venue.image_link = form.image_link.data
+        venue.website = form.website.data
+        venue.seeking_talent = form.seeking_talent.data
+        venue.seeking_description = form.seeking_description.data
+
+        # raise ValueError('A very specific bad thing happened.')
+
+        db.session.commit()
+        flash('Venue ' + venue.name + ' was successfully updated!')
+    except:
+        db.session.rollback()
+        print(sys.exc_info())
+        flash('An error occurred. Venue ' +
+              request.form['name'] + ' could not be updated.')
+    finally:
+        db.session.close()
+
     return redirect(url_for('show_venue', venue_id=venue_id))
 
 #  Create Artist
 #  ----------------------------------------------------------------
 
 
-@ app.route('/artists/create', methods=['GET'])
+@app.route('/artists/create', methods=['GET'])
 def create_artist_form():
     form = ArtistForm()
     return render_template('forms/new_artist.html', form=form)
 
 
-@ app.route('/artists/create', methods=['POST'])
+@app.route('/artists/create', methods=['POST'])
 def create_artist_submission():
+    # TODO use WTForms
     try:
         name = request.form['name']
         city = request.form['city']
@@ -475,9 +526,9 @@ def create_artist_submission():
 #  Shows
 #  ----------------------------------------------------------------
 
-@ app.route('/shows')
+@app.route('/shows')
 def shows():
-    # TODO: Order shows by date. Show past shows? Do I needto aggregate upcoming shows by num?
+    # TODO: Order shows by date. Show past shows? Do I need to aggregate upcoming shows by num?
     # Instructions said to do this but data is not used in view.
     data = []
 
@@ -502,15 +553,16 @@ def shows():
     return render_template('pages/shows.html', shows=data)
 
 
-@ app.route('/shows/create')
+@app.route('/shows/create')
 def create_shows():
     # renders form. do not touch.
     form = ShowForm()
     return render_template('forms/new_show.html', form=form)
 
 
-@ app.route('/shows/create', methods=['POST'])
+@app.route('/shows/create', methods=['POST'])
 def create_show_submission():
+    # TODO use WTForms
     try:
         artist_id = request.form['artist_id']
         venue_id = request.form['venue_id']
@@ -537,12 +589,12 @@ def create_show_submission():
     return render_template('pages/home.html')
 
 
-@ app.errorhandler(404)
+@app.errorhandler(404)
 def not_found_error(error):
     return render_template('errors/404.html'), 404
 
 
-@ app.errorhandler(500)
+@app.errorhandler(500)
 def server_error(error):
     return render_template('errors/500.html'), 500
 
