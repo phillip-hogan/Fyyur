6b7cc0162dd34558177b9737b07e28b37208bd6b
Phillip Hogan <phillip.jr.hogan@gmail.com>
2021-03-12 15:18:26 +0000

    Moved models to own file and added version and packages to requirements text.

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -15,6 +15,7 @@ from flask_wtf import Form
 from forms import *
 import sys
 from datetime import datetime
+from models import *
 #----------------------------------------------------------------------------#
 # App Config.
 #----------------------------------------------------------------------------#
@@ -25,65 +26,6 @@ app.config.from_object('config')
 db = SQLAlchemy(app)
 
 migrate = Migrate(app, db)
-#----------------------------------------------------------------------------#
-# Models.
-#----------------------------------------------------------------------------#
-
-
-class Venue(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    address = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-    website = db.Column(db.String(120))
-    genres = db.Column(db.ARRAY(db.String))
-    seeking_talent = db.Column(db.Boolean, default=False, nullable=False)
-    seeking_description = db.Column(db.Text(), nullable=True)
-    shows = db.relationship('Show', backref="venue",
-                            passive_deletes=True, lazy=True)
-
-    def __repr__(self):
-        return '<Venue {}>'.format(self.name)
-
-
-class Artist(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    genres = db.Column(db.ARRAY(db.String))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-    website = db.Column(db.String(120))
-    seeking_venue = db.Column(db.Boolean, default=False, nullable=False)
-    seeking_description = db.Column(db.Text(), nullable=True)
-    shows = db.relationship('Show', backref="artist",
-                            passive_deletes=True, lazy=True)
-    # Setting the passive_deletes=True prevents SqlAlchemy from NULLing out
-    # the foreign keys.
-    # If they are set to null then you cannot find the child rows in Show to
-    # delete as the ids are gone/null.
-
-    def __repr__(self):
-        return '<Artist {}>'.format(self.name)
-
-
-class Show(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    artist_id = db.Column(db.Integer, db.ForeignKey(
-        'artist.id', ondelete='CASCADE'), nullable=False)
-    venue_id = db.Column(db.Integer, db.ForeignKey(
-        'venue.id', ondelete='CASCADE'), nullable=False)
-    start_time = db.Column(db.DateTime, nullable=False)
-
-    def __repr__(self):
-        return '<Show {}{}>'.format(self.artist_id, self.venue_id)
-
 
 #----------------------------------------------------------------------------#
 # Filters.
